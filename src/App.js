386281003ebd0a777e6136579a5f8f9262d15e28
周小安 <13598091228@139.com>
2018-06-22 15:39:14 +0800

    初始化项目

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Test from './components/Test'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Test />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Test.js
@@ -0,0 +1,9 @@
+import React,{Component} from 'react'
+class Test extends Component {
+    render() {
+        return (
+            <h1>测试</h1>
+        )
+    }
+}
+export default Test
