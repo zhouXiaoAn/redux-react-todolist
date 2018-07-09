9359303f7772c32cc5c78a362cd23a6fa57d892b
周小安 <13598091228@139.com>
2018-07-09 14:11:24 +0800

    webpack文件初始化

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Test from './components/Test'
+import Header from './components/container/header'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Test />
+        <Header />
       </div>
     );
   }
---
+++ src/components/container/header.js
@@ -0,0 +1,9 @@
+import React,{Component} from 'react'
+import './header.css'
+export default class Header extends Component {
+    render() {
+        return (
+            <div className="header">头部</div>
+        )
+    }
+}
